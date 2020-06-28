fcb1e14b574479542cb809b6dc382da7a8bbb6e7
Solrac23 <eduardosantoos573@gmail.com>
2020-06-27 20:37:18 -0400

    config webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  entry: './main.js'
+  entry: './src/main.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js',
+  },
+  devServer:{
+    // será o caminho para abrir o servidor da aplicação
+    contentBase: __dirname + '/public'
+  },
+  module: {
+    rules:[
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      }
+    ],
+  },
 }
